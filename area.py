2c39426664c19fae247e8e01b7b55d8886db1857
Nicholas <nrcar94@gmail.com>
2025-08-03 15:00:58 -0500

    updated based on feedback

--- area.py
+++ area.py
@@ -5,25 +5,25 @@ def circle(radius):
     if float(radius) <= 0:
         raise TypeError
 
-    return round(math.pi * (math.pow(float(radius), 2)), 2)
+    return math.pow(float(radius), 2) * math.pi
 
 def square(side):
 
     if float(side) <= 0:
         raise TypeError
 
-    return round(math.pow(float(side), 2), 2)
+    return math.pow(float(side), 2)
 
 def rectangle(length, width):
 
     if float(length) <= 0 or float(width) <= 0:
         raise TypeError
 
-    return round(float(length)*float(width), 2)
+    return float(length)*float(width)
 
 def triangle(base, height):
 
     if float(base) <= 0 or float(height) <=0:
         raise TypeError
 
-    return round((float(base) * float(height))/2, 2)
+    return (float(base) * float(height))/2
--- test_area.py
+++ test_area.py
@@ -1,71 +1,82 @@
 import unittest
 import area
+import math
 
 class AreaTestCases(unittest.TestCase):
 
     d_v = .01
 
-    def test_string(self):
+    def test_circle(self):
 
         with self.assertRaises(ValueError):
 
             area.circle('one')
 
+        with self.assertRaises(TypeError):
+
+            area.circle(-1)
+
+            area.circle(0)
+
+        self.assertAlmostEqual(area.circle(1), math.pi, delta=self.d_v)
+        self.assertAlmostEqual(area.circle(1.1), 3.80, delta=self.d_v)
+
+    def test_square(self):
+
+        with self.assertRaises(ValueError):
+
             area.square('one')
+
+        with self.assertRaises(TypeError):
+
+            area.square(-1)
+
+            area.square(0)
+
+        self.assertAlmostEqual(area.square(1), 1, delta=self.d_v)
+        self.assertAlmostEqual(area.square(1.1), 1.21, delta=self.d_v)
+
+    def test_rectangle(self):
+
+        with self.assertRaises(ValueError):
 
             area.rectangle('one', 'one')
             area.rectangle('one', 1)
             area.rectangle(1, 'one')
 
-            area.triangle('one', 'one')
-            area.triangle('one', 1)
-            area.triangle(1, 'one')
-
-    def test_negative(self):
-        
         with self.assertRaises(TypeError):
-
-            area.circle(-1)
-
-            area.square(-1)
 
             area.rectangle(-1, -1)
             area.rectangle(1, -1)
             area.rectangle(-1, 1)
 
-            area.triangle(-1, -1)
-            area.triangle(1, -1)
-            area.triangle(-1, 1)
-
-    def test_zero(self):
-        
-        with self.assertRaises(TypeError):
-
-            area.circle(0)
-
-            area.square(0)
-
             area.rectangle(0, 0)
             area.rectangle(1, 0)
             area.rectangle(0, 1)
-
-            area.triangle(0, 0)
-            area.triangle(1, 0)
-            area.triangle(0, 1)
-
-    def test_positive(self):
-        
-        self.assertAlmostEqual(area.circle(1), 3.14, delta=self.d_v)
-        self.assertAlmostEqual(area.circle(1.1), 3.80, delta=self.d_v)
-
-        self.assertAlmostEqual(area.square(1), 1, delta=self.d_v)
-        self.assertAlmostEqual(area.square(1.1), 1.21, delta=self.d_v)
 
         self.assertAlmostEqual(area.rectangle(.1, .2), .02, delta=self.d_v)
         self.assertAlmostEqual(area.rectangle(1.1, 2), 2.2, delta=self.d_v)
         self.assertAlmostEqual(area.rectangle(2, 1.1), 2.2, delta=self.d_v)
         self.assertAlmostEqual(area.rectangle(2, 2), 4, delta=self.d_v)
-        
+
+    def test_triangle(self):
+
+        with self.assertRaises(ValueError):
+
+            area.triangle('one', 'one')
+            area.triangle('one', 1)
+            area.triangle(1, 'one')
+
+        with self.assertRaises(TypeError):
+
+            area.triangle(-1, -1)
+            area.triangle(1, -1)
+            area.triangle(-1, 1)
+
+            area.triangle(0, 0)
+            area.triangle(1, 0)
+            area.triangle(0, 1)
+
         self.assertAlmostEqual(area.triangle(.1, .2), .01, delta=self.d_v)
         self.assertAlmostEqual(area.triangle(1.1, 2), 1.1, delta=self.d_v)
         self.assertAlmostEqual(area.triangle(2, 1.1), 1.1, delta=self.d_v)
