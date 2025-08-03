26c1b1a97dc6a4274ac69bd5e11e4ab8167623f1
Nicholas <nrcar94@gmail.com>
2025-08-03 15:05:45 -0500

    replaced gui and main with correct files

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-from tkinter import *
 from gui import *
-
-def save_to_file(data_dictionary):
-
-    pass
 
 
 def main():
     window = Tk()
-    window.title('Lab 10')
-    window.geometry('240x220')
+    window.title('Lab 11')
+    window.geometry('400x230')
     window.resizable(False, False)
-    new_gui = Gui(window)
+
+    Gui(window)
     window.mainloop()
+
 
 if __name__ == '__main__':
     main()
